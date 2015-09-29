8ae54217b6d6f3f092eb549e42017969b4329529
shark1991 <eakinben@gmail.com>
2015-09-29 14:55:38 -0400

    Update Find-Setid.sh

--- Find-Setid.sh
+++ Find-Setid.sh
@@ -3,4 +3,13 @@
 # the files are displayed using ls -l and sorted by user and group
 #    respectively
 
-find /usr -type f -perm -4000 -exec ls -l {} \; 
+echo
+echo "These files are set-uid"
+echo "======================="
+find /usr -type f -perm -4000 -exec ls -l {} \; | sort -k 3
+
+echo
+echo "These files are set-gid"
+echo "======================="
+find /usr -type -f -perm -2000 -exec ls -l {} \; | sort -k 4
+echo
